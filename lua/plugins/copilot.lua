84f8bb5f9f6ec52a4a995c818de6062356991c2c
Ali Behjati <bahjatia@gmail.com>
2023-10-26 12:23:49 +0200

    fix: customize tab mapping to allow tab when copilot is not there

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,6 +3,16 @@ return function(config)
         return {}
     end
 
+    vim.keymap.set("i", '<Tab>', function()
+        if require("copilot.suggestion").is_visible() then
+            require("copilot.suggestion").accept()
+        else
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+        end
+    end, {
+        silent = true,
+    })
+
     return {
         'zbirenbaum/copilot.lua',
         cmd    = 'Copilot',
@@ -16,9 +26,7 @@ return function(config)
             local opts = {
                 suggestion = {
                     auto_trigger = true,
-                    keymap = {
-                        accept = "<Tab>",
-                    },
+                    accept = false,
                 },
                 filetypes = {
                     ["*"]           = false,
